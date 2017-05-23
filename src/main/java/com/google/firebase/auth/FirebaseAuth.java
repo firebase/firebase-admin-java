c71d70ee04b33869970a52f3356aa1b1593dbc36
hiranya911 <hiranya911@gmail.com>
2017-05-23 13:30:08 -0700

    Made the dependency on google http client explicit

--- src/main/java/com/google/firebase/auth/FirebaseAuth.java
+++ src/main/java/com/google/firebase/auth/FirebaseAuth.java
@@ -318,8 +318,8 @@ public class FirebaseAuth {
     @Override
     public void destroy() {
       // NOTE: We don't explicitly tear down anything here, but public methods of FirebaseAuth
-      // will now fail because calls to getCredential() will hit FirebaseApp.getOptions() which
-      // will throw once the app is deleted.
+      // will now fail because calls to getCredential() and getToken() will hit FirebaseApp,
+      // which will throw once the app is deleted.
     }
   }
 }
