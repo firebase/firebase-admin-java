ed5765826d9df3599b7aa417cc7e31f72cbeb8bb
Avishalom Shalit <avishalom@gmail.com>
2018-02-05 11:54:33 -0500

    clean imports

--- src/test/java/com/google/firebase/auth/UserRecordTest.java
+++ src/test/java/com/google/firebase/auth/UserRecordTest.java
@@ -17,7 +17,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.Date;
 import org.junit.Test;
 
 public class UserRecordTest {
