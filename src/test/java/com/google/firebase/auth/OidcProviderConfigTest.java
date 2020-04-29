9f5d0f0148967afd0eea1e9d3f6df783d3bc63d7
Micah Stairs <micah.stairs@gmail.com>
2020-04-29 10:30:38 -0400

    Fix CreateRequest chaining and provider ID extraction. (#399)
    This makes the base `CreateRequest` setters return the proper instance type, so that methods can be chained. This also makes it so that the provider ID can be parsed from the resource name.
    
    A package private `getProviderId()` method was also added, which will be needed by the `FirebaseUserManager` class when the provider config operations are added there.
    
    Also renamed `AuthProviderConfig` to `ProviderConfig` since "Auth" is redundant with the package name.
    
    This work is part of adding multi-tenancy support (see issue #332).

--- src/test/java/com/google/firebase/auth/OidcProviderConfigTest.java
+++ src/test/java/com/google/firebase/auth/OidcProviderConfigTest.java
@@ -16,9 +16,60 @@
 
 package com.google.firebase.auth;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.api.client.googleapis.util.Utils;
+import com.google.api.client.json.JsonFactory;
+import java.io.IOException;
+import java.util.Map;
 import org.junit.Test;
 
 public class OidcProviderConfigTest {
+
+  private static final JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
+
+  private static final String OIDC_JSON_STRING =
+      "{"
+        + "\"name\":\"projects/projectId/oauthIdpConfigs/oidc.provider-id\","
+        + "\"displayName\":\"DISPLAY_NAME\","
+        + "\"enabled\":true,"
+        + "\"clientId\":\"CLIENT_ID\","
+        + "\"issuer\":\"https://oidc.com/issuer\""
+        + "}";
+
+  @Test
+  public void testJsonSerialization() throws IOException {
+    OidcProviderConfig config = jsonFactory.fromString(OIDC_JSON_STRING, OidcProviderConfig.class);
+
+    assertEquals(config.getProviderId(), "oidc.provider-id");
+    assertEquals(config.getDisplayName(), "DISPLAY_NAME");
+    assertTrue(config.isEnabled());
+    assertEquals(config.getClientId(), "CLIENT_ID");
+    assertEquals(config.getIssuer(), "https://oidc.com/issuer");
+  }
+
+  @Test
+  public void testCreateRequest() throws IOException {
+    OidcProviderConfig.CreateRequest createRequest = new OidcProviderConfig.CreateRequest();
+    createRequest
+      .setProviderId("oidc.provider-id")
+      .setDisplayName("DISPLAY_NAME")
+      .setEnabled(false)
+      .setClientId("CLIENT_ID")
+      .setIssuer("https://oidc.com/issuer");
+
+    assertEquals("oidc.provider-id", createRequest.getProviderId());
+    Map<String,Object> properties = createRequest.getProperties();
+    assertEquals(properties.size(), 4);
+    assertEquals("DISPLAY_NAME", (String) properties.get("displayName"));
+    assertFalse((boolean) properties.get("enabled"));
+    assertEquals("CLIENT_ID", (String) properties.get("clientId"));
+    assertEquals("https://oidc.com/issuer", (String) properties.get("issuer"));
+  }
+
   @Test(expected = IllegalArgumentException.class)
   public void testInvalidIssuerUrl() {
     new OidcProviderConfig.CreateRequest().setIssuer("not a valid url");
