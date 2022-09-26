2846f47a32534f59b4f53cf89e93b51f8038a8be
Tom Andersen <tomandersen@google.com>
2022-09-26 16:57:10 -0400

    Implement MultiDB Support

--- src/main/java/com/google/firebase/cloud/FirestoreClient.java
+++ src/main/java/com/google/firebase/cloud/FirestoreClient.java
@@ -14,6 +14,10 @@ import com.google.firebase.internal.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * {@code FirestoreClient} provides access to Google Cloud Firestore. Use this API to obtain a
  * <a href="https://googlecloudplatform.github.io/google-cloud-java/google-cloud-clients/apidocs/com/google/cloud/firestore/Firestore.html">{@code Firestore}</a>
@@ -32,7 +36,7 @@ public class FirestoreClient {
 
   private final Firestore firestore;
 
-  private FirestoreClient(FirebaseApp app) {
+  private FirestoreClient(FirebaseApp app, String databaseId) {
     checkNotNull(app, "FirebaseApp must not be null");
     String projectId = ImplFirebaseTrampolines.getProjectId(app);
     checkArgument(!Strings.isNullOrEmpty(projectId),
@@ -47,12 +51,13 @@ public class FirestoreClient {
         .setCredentialsProvider(
             FixedCredentialsProvider.create(ImplFirebaseTrampolines.getCredentials(app)))
         .setProjectId(projectId)
+        .setDatabaseId(databaseId)
         .build()
         .getService();
   }
 
   /**
-   * Returns the Firestore instance associated with the default Firebase app. Returns the same
+   * Returns the default Firestore instance associated with the default Firebase app. Returns the same
    * instance for all invocations. The Firestore instance and all references obtained from it
    * becomes unusable, once the default app is deleted.
    *
@@ -65,7 +70,7 @@ public class FirestoreClient {
   }
 
   /**
-   * Returns the Firestore instance associated with the specified Firebase app. For a given app,
+   * Returns the default Firestore instance associated with the specified Firebase app. For a given app,
    * always returns the same instance. The Firestore instance and all references obtained from it
    * becomes unusable, once the specified app is deleted.
    *
@@ -75,32 +80,86 @@ public class FirestoreClient {
    */
   @NonNull
   public static Firestore getFirestore(FirebaseApp app) {
-    return getInstance(app).firestore;
+    return getFirestore(app, ImplFirebaseTrampolines.getFirestoreOptions(app).getDatabaseId());
   }
 
-  private static synchronized FirestoreClient getInstance(FirebaseApp app) {
+  /**
+   * Returns the Firestore instance associated with the specified Firebase app. For a given app,
+   * always returns the same instance. The Firestore instance and all references obtained from it
+   * becomes unusable, once the specified app is deleted.
+   *
+   * @param app A non-null {@link FirebaseApp}.
+   * @param database - The name of database.
+   * @return A non-null <a href="https://googlecloudplatform.github.io/google-cloud-java/google-cloud-clients/apidocs/com/google/cloud/firestore/Firestore.html">{@code Firestore}</a>
+   *     instance.
+   */
+  @NonNull
+  public static Firestore getFirestore(FirebaseApp app, String database) {
+    return getInstance(app, database).firestore;
+  }
+
+  /**
+   * Returns the Firestore instance associated with the default Firebase app. Returns the same
+   * instance for all invocations. The Firestore instance and all references obtained from it
+   * becomes unusable, once the default app is deleted.
+   *
+   * @param database - The name of database.
+   * @return A non-null <a href="https://googlecloudplatform.github.io/google-cloud-java/google-cloud-clients/apidocs/com/google/cloud/firestore/Firestore.html">{@code Firestore}</a>
+   *     instance.
+   */
+  @NonNull
+  public static Firestore getFirestore(String database) {
+    return getFirestore(FirebaseApp.getInstance(), database);
+  }
+
+  private static synchronized FirestoreClient getInstance(FirebaseApp app, String database) {
     FirestoreClientService service = ImplFirebaseTrampolines.getService(app,
         SERVICE_ID, FirestoreClientService.class);
     if (service == null) {
       service = ImplFirebaseTrampolines.addService(app, new FirestoreClientService(app));
     }
-    return service.getInstance();
+    return service.getInstance().get(database);
   }
 
   private static final String SERVICE_ID = FirestoreClient.class.getName();
 
-  private static class FirestoreClientService extends FirebaseService<FirestoreClient> {
+  private static class FirestoreClientService extends FirebaseService<FirestoreInstances> {
 
     FirestoreClientService(FirebaseApp app) {
-      super(SERVICE_ID, new FirestoreClient(app));
+      super(SERVICE_ID, new FirestoreInstances(app));
     }
 
     @Override
     public void destroy() {
-      try {
-        instance.firestore.close();
-      } catch (Exception e) {
-        logger.warn("Error while closing the Firestore instance", e);
+      instance.destroy();
+    }
+  }
+
+  private static class FirestoreInstances {
+
+    private final FirebaseApp app;
+
+    private final Map<String, FirestoreClient> clients =
+      Collections.synchronizedMap(new HashMap<>());
+
+    private FirestoreInstances(FirebaseApp app) {
+      this.app = app;
+    }
+
+    FirestoreClient get(String databaseId) {
+      return clients.computeIfAbsent(databaseId, id -> new FirestoreClient(app, id));
+    }
+
+    void destroy() {
+      synchronized (clients) {
+        for (FirestoreClient client : clients.values()) {
+          try {
+            client.firestore.close();
+          } catch (Exception e) {
+            logger.warn("Error while closing the Firestore instance", e);
+          }
+        }
+        clients.clear();
       }
     }
   }
