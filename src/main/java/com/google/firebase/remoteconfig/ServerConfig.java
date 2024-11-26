02c3cd516f56deafbd371aad42757cd53288748d
Athira M <athiramanu@google.com>
2024-11-26 10:32:43 +0530

    Implementation for evaluation of server side remote config

---
+++ src/main/java/com/google/firebase/remoteconfig/ServerConfig.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.remoteconfig;
+
+import java.util.Map;
+
+/**
+ * Represents the configuration produced by evaluating a server template.
+ */
+public class ServerConfig {
+    private final Map<String, Value> configValues;
+
+    ServerConfig(Map<String, Value> configValues) {
+        this.configValues = configValues;
+    }
+
+    /**
+     * Gets the value for the given key as a string. Convenience method for calling
+     * serverConfig.getValue(key).asString().
+     * 
+     * @param key
+     * @return config value for the given key as string.
+     */
+    public String getString(String key) {
+        return configValues.get(key).asString();
+    }
+
+    /**
+     * Gets the value for the given key as a boolean.Convenience method for calling
+     * serverConfig.getValue(key).asBoolean().
+     * 
+     * @param key
+     * @return config value for the given key as boolean.
+     */
+    public boolean getBoolean(String key) {
+        return Boolean.parseBoolean(getString(key));
+    }
+
+    /**
+     * Gets the value for the given key as long.Convenience method for calling
+     * serverConfig.getValue(key).asLong().
+     * 
+     * @param key
+     * @return config value for the given key as long.
+     */
+    public long getLong(String key) {
+        return Long.parseLong(getString(key));
+    }
+
+    /**
+     * Gets the value for the given key as double.Convenience method for calling
+     * serverConfig.getValue(key).asDouble().
+     * 
+     * @param key
+     * @return config value for the given key as double.
+     */
+    public double getDouble(String key) {
+        return Double.parseDouble(getString(key));
+    }
+
+}
---
+++ src/main/java/com/google/firebase/remoteconfig/ServerTemplateData.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.remoteconfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import com.google.firebase.internal.NonNull;
+
+/**
+ * Represents data stored in a server side Remote Config template.
+ */
+public class ServerTemplateData {
+    private String etag;
+    private Map<String, Parameter> parameters;
+    private Map<String, OneOfCondition> conditions;
+    private Version version;
+
+    /**
+     * Creates a new {@link ServerTemplateData} object.
+     * 
+     * @param etag The ETag of this template.
+     */
+    public ServerTemplateData(String etag) {
+        this.parameters = new HashMap<>();
+        this.conditions = new HashMap<>();
+        this.etag = etag;
+    }
+
+    /**
+     * Creates a new {@link ServerTemplateData} object with an empty etag.
+     */
+    public ServerTemplateData() {
+        this((String) null);
+    }
+
+    /**
+     * Gets the map of parameters of the server side template data.
+     *
+     * @return A non-null map of parameter keys to their optional default values and
+     *         optional
+     *         conditional values.
+     */
+    @NonNull
+    public Map<String, Parameter> getParameters() {
+        return this.parameters;
+    }
+
+    /**
+     * Gets the map of parameters of the server side template data.
+     *
+     * @return A non-null map of condition keys to their conditions.
+     */
+    @NonNull
+    public Map<String, OneOfCondition> getConditions() {
+        return this.conditions;
+    }
+
+    /**
+     * Gets the ETag of the server template data.
+     *
+     * @return The ETag of the server template data.
+     */
+    public String getETag() {
+        return this.etag;
+    }
+
+    /**
+     * Gets the version information of the template.
+     *
+     * @return The version information of the template.
+     */
+    public Version getVersion() {
+        return version;
+    }
+
+    /**
+     * Sets the map of parameters of the server side template data.
+     *
+     * @param parameters A non-null map of parameter keys to their optional default
+     *                   values and
+     *                   optional conditional values.
+     * @return This {@link ServerTemplateData} instance.
+     */
+    public ServerTemplateData setParameters(
+            @NonNull Map<String, Parameter> parameters) {
+        checkNotNull(parameters, "parameters must not be null.");
+        this.parameters = parameters;
+        return this;
+    }
+
+    /**
+     * Sets the map of conditions of the server side template data.
+     *
+     * @param conditions A non-null map of conditions.
+     * @return This {@link ServerTemplateData} instance.
+     */
+    public ServerTemplateData setConditions(
+            @NonNull Map<String, OneOfCondition> conditions) {
+        checkNotNull(conditions, "conditions must not be null.");
+        this.conditions = conditions;
+        return this;
+    }
+
+    /**
+     * Sets the version information of the template.
+     * Only the version's description field can be specified here.
+     *
+     * @param version A {@link Version} instance.
+     * @return This {@link Template} instance.
+     */
+    public ServerTemplateData setVersion(Version version) {
+        this.version = version;
+        return this;
+    }
+}
